28c6585ebf78286853720c75bc364dc6030120fc
alk <konovalov.phone@gmail.com>
2022-04-21 17:27:24 +0300

    new tarif

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const Config = {
-    tarifid: 179835913, // Агрегатор скидок
+    // tarifid: 179835913, // Агрегатор скидок 
+    tarifid: 189574980, // Агрегатор скидок 2022
 }
 
 export default Config;
